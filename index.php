ad4190e535a3363c6be2bacc1ac30e9da59c1c72
xF1owx <florian.r@codeur.online>
2018-06-08 10:26:58 +0200

    base folders

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+$action = ( isset( $_GET['action']) )? $_GET['action'] : "home";
+
+switch ($action){
+
+case 'home':
+require_once('controllers/home.php');
+break;
+
+default:
+require_once('controllers/home.php');
+}
