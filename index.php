55bce45cc1526d37232848b6e4df7139e0ee2e9b
Z-dorf <sebastian.zanon2@gmail.com>
2018-06-11 14:32:23 +0200

    update rooter

--- index.php
+++ index.php
@@ -4,50 +4,30 @@ $action = ( isset( $_GET['action']) )? $_GET['action'] : "home";
 
 switch ($action){
 
-case 'home':
-require_once('controllers/home.php');
-break;
+    case 'Goworking' :
+    case '/Goworking/' :
+        require_once('controllers/home.php');
+        break;
 
-default:
-require_once('controllers/home.php');
-}
+    case 'about':
+        require_once('controllers/aboutUs.php');
+        break;
+
+    case 'coworking':
+        require_once('controllers/coworking.php');
+        break;
+
+    case 'contact':
+        require_once('controllers/contact.php');
+        break;
+
+    case 'wifiLib':
+        require_once('controllers/wifiLib.php');
+        break;
 
 
+    default:
+        require_once('controllers/home.php');
+    }
 
-
-// $url = explode("/",$_SERVER['REQUEST_URI'],4);
-// if (count($url)>3){
-//     array_pop($url);
-// }
-// $path = implode("/", $url);
-
-// switch($path){
-//     case "/meme-generator":
-//     case "/meme-generator/" :
-//         require('controllers/ctrlHomepage.php');
-//         break;
-//     case "/meme-generator/homepage":
-//         require('controllers/ctrlHomepage.php');
-//         break;
-//     case "/meme-generator/create":
-//         require('controllers/ctrlCreate.php');
-//         break;
-//     case "/meme-generator/browse":
-//         require('controllers/ctrlBrowse.php');
-//         break;
-//     case "/meme-generator/browse/meme":
-//         require('controllers/ctrldownload.php');
-//         break;
-//     case "/meme-generator/upload":
-//         require('controllers/ctrlUpload.php');
-//         break;
-//     case "/meme-generator/edit-meme":
-//         require('controllers/ctrlEditMeme.php');
-//         break;
-//     case "/meme-generator/upload-meme":
-//         require('controllers/ctrlUpload-meme.php');
-//         break;
-//     case "/meme-generator/meme":
-//         require('controllers/ctrlMeme.php');
-//         break;
-// }
+?>
