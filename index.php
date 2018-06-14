a975a51882e0fcaaf2e3314360aa8a5a00e5a411
Z-dorf <sebastian.zanon2@gmail.com>
2018-06-14 15:27:24 +0200

    breaks

--- index.php
+++ index.php
@@ -28,10 +28,12 @@ switch ($action){
         break;
 
     case 'main';
-    require_once('controllers/main.php');
+        require_once('controllers/main.php');
+        break;
 
     default:
         require_once('controllers/home.php');
+        break;
     }
 
 ?>
