8fba362ee15c219d8252ef976c773490707f175f
Z-dorf <sebastian.zanon2@gmail.com>
2018-06-19 10:49:19 +0200

    ?

--- utils/db.php
+++ utils/db.php
@@ -1,7 +1,8 @@
 <?php
 try {
     //rajouter ici votre PDO phpmyadmin promo 17
-    $bdd = new PDO('mysql:localhost;dbname=goworkin_bdd;charset=utf8', 'sirine', 'online@2017');
+    // $bdd = new PDO('mysql:localhost;dbname=goworkin;charset=utf8', 'admin', 'passwordMariaDB');
+    $bdd = new PDO('mysql:sebastianz@localhost;dbname=goworkin;charset=utf8', 'sebastianz', 'NM83uHiGZQ');
     // echo 'successgit';
   
 }
