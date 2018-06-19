fae10d48b8851f974c03e6180f83275dac4dba9f
Z-dorf <sebastian.zanon2@gmail.com>
2018-06-19 15:30:07 +0200

    rip ignore

--- utils/db.php
+++ utils/db.php
@@ -1,14 +1,13 @@
 <?php
+
 try {
     //rajouter ici votre PDO phpmyadmin promo 17
-    $bdd = new PDO('mysql:localhost;dbname=goworkin;charset=utf8', 'admin', 'passwordMariaDB');
+    $bdd = new PDO('mysql:xxxxxxxxxx;dbname=xxxxxx;charset=utf8', 'xxxxxx', 'xxxxxx');
     // echo 'successgit';
-  
 }
+
 catch(Exception $e) {
     die('Erreur:'.$e->getMessage());
 }
-
-// $bdd = new PDO('mysql:localhost;dbname=goworkin;charset=utf8', 'admin', 'passwordMariaDB');
 
 ?>
