fb482de0ceb01b37aea09f6ea77236b6fb6562fd
Z-dorf <sebastian.zanon2@gmail.com>
2018-06-19 09:33:47 +0200

    fixing ignore

--- utils/db.php
+++ utils/db.php
@@ -1,7 +1,8 @@
 <?php
 try {
     //rajouter ici votre PDO phpmyadmin promo 17
-    $bdd = new PDO('mysql:localhost;dbname=goworkin;charset=utf8', 'admin', 'passwordMariaDB');
+    // $bdd = new PDO('mysql:localhost;dbname=goworkin;charset=utf8', 'admin', 'passwordMariaDB');
+
     // echo 'successgit';
   
 }
