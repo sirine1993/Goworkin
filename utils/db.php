9b412a013816cf59d59476e35475a8c9e5c76b65
sirine <stagiaire@PORT0364>
2018-06-19 09:56:32 +0200

    refonte page home 2

--- utils/db.php
+++ utils/db.php
@@ -1,7 +1,7 @@
 <?php
 try {
     //rajouter ici votre PDO phpmyadmin promo 17
-    $bdd = new PDO('mysql:localhost;dbname=goworkin;charset=utf8', 'admin', 'passwordMariaDB');
+    $bdd = new PDO('mysql:localhost;dbname=goworkin_bdd;charset=utf8', 'sirine', 'online@2017');
     // echo 'successgit';
   
 }
