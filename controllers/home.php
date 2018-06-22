db600ecd8a1579e005644acb3ba82ba344d6464f
sirine <stagiaire@PORT0364>
2018-06-22 10:40:02 +0200

    bug

--- controllers/home.php
+++ controllers/home.php
@@ -1,8 +1,5 @@
 <?php
-<<<<<<< HEAD
 //echo 'coucou home ';
-=======
->>>>>>> b9ccf532f1720969fb298d2a63ab01a19db1b15e
 
 require_once 'vendor/autoload.php';
 require_once('models/request.php');
--- utils/db.php
+++ utils/db.php
@@ -1,12 +1,6 @@
 <?php
-<<<<<<< HEAD
 
 try{
-=======
-try {
-    $bdd = new PDO('mysql:host=localhost;dbname=goworking;charset=utf8', 'admin', 'Ucenje2018');
-    // echo 'successgit';
->>>>>>> b9ccf532f1720969fb298d2a63ab01a19db1b15e
   
     $bdd=new PDO('mysql:host=localhost;dbname=goworkin_bdd;charset=utf8','sirine','online@2017');
 }
@@ -14,11 +8,5 @@ catch(Exeption $e)
 {      
 	die('Erreur:'.$e->getMessage());
 }
-<<<<<<< HEAD
     
 
-=======
- //rajouter ici votre PDO phpmyadmin promo 17
-// $bdd = new PDO('mysql:localhost;dbname=goworkin;charset=utf8', 'admin', 'passwordMariaDB');
-?>
->>>>>>> b9ccf532f1720969fb298d2a63ab01a19db1b15e
